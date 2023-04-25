d83fe4451bae6ee47d21356970894a550ca36320
abdulhp <hpabdul902@gmail.com>
2023-04-25 11:23:29 +0700

    NotesHandler: postNoteHandler

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -3,4 +3,41 @@ class NotesHandler {
   constructor(service) {
     this._service = service;
   }
+
+  postNoteHandler(request, h) {
+    try {
+      const { title = 'untitled', body, tags } = request.payload;
+
+      const noteId = this._service.addNote({ title, body, tags });
+
+      return h.response({
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
+        data: {
+          noteId,
+        },
+      });
+    } catch (error) {
+      return h.response({
+        status: 'fail',
+        message: error.message,
+      }).code(400);
+    }
+  }
+
+  getNotesHandler() {
+
+  }
+
+  getNoteByIdHandler() {
+
+  }
+
+  putNoteByIdHandler() {
+
+  }
+
+  deleteNoteByIdHandler() {
+
+  }
 }
