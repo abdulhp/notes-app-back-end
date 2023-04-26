651877c336a9374ba02e1ea2a89aaddb59c1f39c
abdulhp <hpabdul902@gmail.com>
2023-04-26 09:54:31 +0700

    Init postgresql and dotenv

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./src/api/notes');
 const NotesService = require('./src/services/inMemory/NotesService');
@@ -5,8 +7,8 @@ const NotesValidator = require('./src/validator/notes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
